05b5e1a6fb0162a81fd56d11c059b7e51c3c64a4
MariaPaschevska <maria.paschevska@gmail.com>
2018-10-08 15:43:54 +0300

    Shared  module added

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {ProductListComponent} from './product-list.component';
 import {ConvertToSpacesPipe} from '../shared/convert-to-spaces.pipe';
-import {StarComponent} from '../shared/star.component';
 import {ProductDetailComponent} from './product-detail.component';
-import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {ProductGuardService} from './product-guard.service';
 import {WelcomeComponent} from '../home/welcome.component';
 import {ProductService} from './product.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { path: 'products/:id',
@@ -22,12 +18,12 @@ import {ProductService} from './product.service';
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    ]),
+    SharedModule
   ],
   declarations: [
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent,
     ProductDetailComponent
   ],
   providers: [
